6c4fd33da1997c4549bf14385f8648870205c217
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-11-08 22:19:25 +0900

    Q010 기초 입출력 - 정수 1개 입력받아 그대로 출력하기

---
+++ src/questions/q_010.java
@@ -0,0 +1,20 @@
+// 기초 입출력 : 정수 1개 입력받아 그대로 출력하기
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_010 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		
+		System.out.println(a);
+		
+	}
+
+}
