9bdb2b24d2b44f680c50436503d9ef5e18591260
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-11-09 02:34:08 +0900

    Q012 기초 입출력 - 실수 1개 입력받아 그대로 출력하기

---
+++ src/questions/q_012.java
@@ -0,0 +1,20 @@
+// 기초 입출력 : 실수 1개 입력받아 그대로 출력하기
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_012 {
+
+	public static void main(String[] args) {
+		
+		float a;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextFloat();
+		
+		System.out.println(String.format("%.6f", a));
+
+	}
+
+}
