5a03fbfcde0f640caab9f5a33276233bff99edd2
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-11-20 08:39:49 +0900

    Q026 기초 입출력 - 시분초 입력받아 분만 출력하기 : 완료 전

--- src/questions/q_026.java
+++ src/questions/q_026.java
@@ -2,11 +2,21 @@
 
 package questions;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class q_026 {
 
 	public static void main(String[] args) {
+		
+		String line;
+		
+		Scanner sc = new Scanner(System.in);
+		line = sc.nextLine();
+		
+		DateFormat formatter = new SimpleDateFormat("hh:mm:ss");
 	
 	}
 
