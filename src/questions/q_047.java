f364bc7ff1a43e0c5ad3e3ab67996f5fcb2ad88d
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-11-29 07:13:56 +0900

    Q047 기초 비트시프트연산 - 정수 1개 입력받아 2배 곱해 출력하기

---
+++ src/questions/q_047.java
@@ -0,0 +1,21 @@
+// 기초 비트시프트연산 : 정수 1개 입력받아 2배 곱해 출력하기
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_047 {
+
+	public static void main(String[] args) {
+		
+		int input;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		input = sc.nextInt();
+		
+		System.out.println(input<<1);
+		
+	}
+
+}
