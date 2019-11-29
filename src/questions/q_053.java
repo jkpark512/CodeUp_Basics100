e6f83da44791985e04162e301a5db831978a10fc
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-11-30 07:36:52 +0900

    Q053 기초 논리연산 - 참 거짓 바꾸기 : 완료 전

---
+++ src/questions/q_053.java
@@ -0,0 +1,22 @@
+// 기초 논리연산 : 참 거짓 바꾸기
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_053 {
+
+	public static void main(String[] args) {
+		
+		int input;
+		int output;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		input = sc.nextInt();
+		output = !input;
+		
+		System.out.println();
+	}
+
+}
