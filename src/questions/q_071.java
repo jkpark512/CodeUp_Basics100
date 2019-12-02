aa0ad551b83a4314daa0fba102985eb1457a0857
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-12-03 08:12:12 +0900

    Q071 기초 반복실행구조 - 0 입력될 때까지 무한 출력하기1 : 완료 전

---
+++ src/questions/q_071.java
@@ -0,0 +1,16 @@
+// 기초 반복실행구조 : 0 입력될 때까지 무한 출력하기1
+
+package questions;
+
+public class q_071 {
+
+	public static void main(String[] args) {
+		
+		int[] array = {0,0,0};
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
