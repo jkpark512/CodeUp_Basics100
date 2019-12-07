fc6b35c948ad0c551bb3cbc1f075fedf3facf386
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-12-07 16:20:18 +0900

    Q090 기초 종합 - 수 나열하기2 : 완료 전

---
+++ src/questions/q_090.java
@@ -0,0 +1,24 @@
+// 기초 종합 : 수 나열하기2
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_090 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		int r;
+		int n;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		r = sc.nextInt();
+		n = sc.nextInt();
+		
+
+	}
+
+}
