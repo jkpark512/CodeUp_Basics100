0a7bb5175e1bf007276eb9f8e1f4e9e87736ebab
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-12-16 14:16:29 +0900

    Q030 기초 데이터형 - 정수 1개 입력받아 그대로 출력하기3

---
+++ src/questions/q_030.java
@@ -0,0 +1,21 @@
+// 기초 데이터형 : 정수 1개 입력받아 그대로 출력하기3
+
+package questions;
+
+import java.util.Scanner;
+
+public class q_030 {
+
+	public static void main(String[] args) {
+		
+		long input;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		input = sc.nextLong();
+		
+		System.out.println(input);
+
+	}
+
+}
