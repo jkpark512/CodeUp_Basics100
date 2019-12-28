f6a5417cd52549e467e7f781e063f8b4838c380a
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2019-12-29 01:20:41 +0900

    Q079 기초 종합 - 원하는 문자가 입력될 때까지 반복 출력하기 : 완료 전

--- src/questions/q_079.java
+++ src/questions/q_079.java
@@ -1,10 +1,31 @@
+// 기초 종합 : 원하는 문자가 입력될 때까지 반복 출력하기
+
 package questions;
+
+import java.util.Scanner;
 
 public class q_079 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String array;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		array = sc.next();
+		
+		int i = 1;
+		int k;
+		
+		while(i==1) {
+			
+			for(k=0; k<k+1; k++) {
+				System.out.println(array[k]);
+			}
+			
+		}
+		
+		
 	}
 
 }
