ca62150f9abd50b7850f9642aa61fd1a5f33c617
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2020-01-14 00:21:02 +0900

    Q092 기초 종합 - 함께 문제 푸는 날 : 완료

--- src/questions/q_092.java
+++ src/questions/q_092.java
@@ -8,6 +8,20 @@ public class q_092 {
 
 	public static void main(String[] args) {
 		
+		int human1, human2, human3;
+		int day = 1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		human1 = sc.nextInt();
+		human2 = sc.nextInt();
+		human3 = sc.nextInt();
+		
+		while(day%human1 != 0 || day%human2 != 0 || day%human3 != 0) {
+			day++;
+		}
+		
+		System.out.println(day);
 		
 
 	}
