f88831857c2309df49bd4b1edfa14fc59ee191cb
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2020-03-10 00:14:13 +0900

    Q070 기초 조건/선택실행구조 - 월 입력받아 계절 출력하기 : 완료

--- src/questions/q_070.java
+++ src/questions/q_070.java
@@ -2,10 +2,41 @@
 
 package questions;
 
+import java.util.Scanner;
+
 public class q_070 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int month;
+		
+		Scanner sc = new Scanner(System.in);
+		month = sc.nextInt();
+		
+		switch(month) {
+			case 12:
+			case 1:
+			case 2:
+				System.out.println("winter");
+			break;
+			case 3:
+			case 4:
+			case 5:
+				System.out.println("spring");
+			break;
+			case 6:
+			case 7:
+			case 8:
+				System.out.println("summer");
+			break;
+			case 9:
+			case 10:
+			case 11:
+				System.out.println("fall");
+			break;
+		}
+		
+		sc.close();
 
 	}
 
